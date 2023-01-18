bd144fec82b6662e22a8a50bdcaad479f51ba165
Average Female <MeeperReaper@gmail.com>
2023-01-18 07:43:04 +0000

    rahhhhhh

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ async function main() {
     if (user.toLowerCase() == "averagefemale_" || user.toLowerCase() == "je_remy_") {
       if (text.toLowerCase() == "!toggle on") {
         if (bool) return chatClient.say(channel,"Already on. [Automated Response]",{replyTo: msg})
-        bool = false
+        bool = true
         return chatClient.say(channel,"Turned on. [Automated Response]",{replyTo: msg})
       } else if (text.toLowerCase() == "!toggle off") {
         if (!bool) return chatClient.say(channel,"Already off. [Automated Response]",{replyTo: msg})
-        bool = true
+        bool = false
         return chatClient.say(channel,"Turned off. [Automated Response]",{replyTo: msg})
       }
       if (text.toLowerCase().startsWith("!loop")) {
@@ -35,6 +35,7 @@ async function main() {
         }
       }
     }
+    console.log(bool,text)
     if (!bool) return 
     if (/can u donat|please donat|dono me|pls donat|gift me|dono pls|pls dono|can u dono|can you dono|donate pls|donated pls|can you donat|donate me|donated me|donate to me/g.test(text.toLowerCase())) {
       chatClient.say(channel,`He is currently not donating at the moment. [Automated Response]`,{replyTo: msg})
